d715a9e73e78e43d30cc8220158c636fb3578dd0
Phreakester <gettyv05@gmail.com>
2024-04-24 13:45:46 -0700

    setup test class format

---
+++ lib/Test.py
@@ -0,0 +1,13 @@
+class Test:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f'Kaguya-sama: {self.name}'
+
+    def test_input(self):
+        return 'test_input'
+    
+    def test_output(self):
+        return 'test_output'
+    
